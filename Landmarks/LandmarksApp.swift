a51f3348bc9332431d640019b71f1213ec3c037e
Sergey Leonov <brottys@gmail.com>
2021-04-18 12:27:04 +0300

    Chapter 1 completed

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by BrottyS on 17.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
